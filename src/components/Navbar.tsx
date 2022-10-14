fb0006392e0a9704794b89c805f8617b7f81e58b
Krystian Ogonowski <tailowskikrystian@gmail.com>
2022-10-15 00:23:49 +0200

    prevent navbar and footer from displaying on some pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import BottomNav from "./Navbar/BottomNav";
 import SideNav from "./Navbar/SideNav";
 import { useWindowSize } from "usehooks-ts";
+import { useLocation } from "react-router-dom";
+
+const excludedLocations = ["/signin", "/signup", "/restore", "/Unauthorized"];
 
 const Navbar = () => {
 	const { width } = useWindowSize();
+	const location = useLocation();
+
+	// prevents navbar from rendering on excluded locations
+	if (excludedLocations.includes(location.pathname)) return null;
 
 	return <>{width > 768 ? <SideNav /> : <BottomNav />}</>;
 };
